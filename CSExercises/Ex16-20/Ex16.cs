2c038f45273138e6bf0ff925db732b152a17d868
shaunlau <e0001417@u.nus.edu>
2015-08-17 12:22:15 +0800

    Final Ex16

--- CSExercises/Ex16-20/Ex16.cs
+++ CSExercises/Ex16-20/Ex16.cs
@@ -1,10 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
 namespace CSExercises
 {
     public class Ex16
     {
         public static void Main(string[] args)
         {
+            Console.Write("Please provide your name: ");
+            string UserName = Console.ReadLine();
+            Console.Write("Please enter your gender: ");
+            string gender = Console.ReadLine();         
+
+            if (gender == "M")             
+                Console.WriteLine("Good Morning Mr. " + UserName);
+            else if (gender == "F")              
+                Console.WriteLine("Good Morning Ms. " + UserName);
+            else
+                Console.WriteLine("Good Morning");
             
+
         }
     }
 }
